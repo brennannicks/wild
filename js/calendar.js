59e5f59a263af1f29385966abb47d1e40a399ff0
Darren Jack <darrencharlesjack79@gmail.com>
2016-05-13 16:05:30 -0600

    moved over changes from production, took out calendar and added another section

--- js/calendar.js
+++ js/calendar.js
@@ -1,6 +1,6 @@
 // Your Client ID can be retrieved from your project in the Google
 // Developer Console, https://console.developers.google.com
-var CLIENT_ID = '<212849857764-g0qrr06rggbh8l6rit4io9uq008h8ggo.apps.googleusercontent.com>';
+var CLIENT_ID = '212849857764-g0qrr06rggbh8l6rit4io9uq008h8ggo.apps.googleusercontent.com';
 
 var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 

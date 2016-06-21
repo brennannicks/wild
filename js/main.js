d28b0fc6dfd5be41351f0c6904307b61e6cc3613
Darren Jack <darrencharlesjack79@gmail.com>
2016-06-20 21:36:50 -0600

    added in trailer section, changed snazzy maps, optimized features list

--- js/main.js
+++ js/main.js
@@ -53,98 +53,7 @@ function init() {
         // styles: [{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#b5cbe4"}]},{"featureType":"landscape","stylers":[{"color":"#efefef"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#83a5b0"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#bdcdd3"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e3eed3"}]},{"featureType":"administrative","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"road"},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{},{"featureType":"road","stylers":[{"lightness":20}]}],
         //END original *****************
 styles:
-[
-{
-"featureType": "administrative",
-"elementType": "all",
-"stylers": [
-    {
-        "visibility": "on"
-    },
-    {
-        "lightness": 33
-    }
-]
-},
-{
-"featureType": "landscape",
-"elementType": "all",
-"stylers": [
-    {
-        "color": "#f2e5d4"
-    }
-]
-},
-{
-"featureType": "poi.park",
-"elementType": "geometry",
-"stylers": [
-    {
-        "color": "#c5dac6"
-    }
-]
-},
-{
-"featureType": "poi.park",
-"elementType": "labels",
-"stylers": [
-    {
-        "visibility": "on"
-    },
-    {
-        "lightness": 20
-    }
-]
-},
-{
-"featureType": "road",
-"elementType": "all",
-"stylers": [
-    {
-        "lightness": 20
-    }
-]
-},
-{
-"featureType": "road.highway",
-"elementType": "geometry",
-"stylers": [
-    {
-        "color": "#c5c6c6"
-    }
-]
-},
-{
-"featureType": "road.arterial",
-"elementType": "geometry",
-"stylers": [
-    {
-        "color": "#e4d7c6"
-    }
-]
-},
-{
-"featureType": "road.local",
-"elementType": "geometry",
-"stylers": [
-    {
-        "color": "#fbfaf7"
-    }
-]
-},
-{
-"featureType": "water",
-"elementType": "all",
-"stylers": [
-    {
-        "visibility": "on"
-    },
-    {
-        "color": "#acbcc9"
-    }
-]
-}
-],
+[{"featureType":"water","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#aee2e0"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#abce83"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#769E72"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#7B8758"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"color":"#EBF4A4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"visibility":"simplified"},{"color":"#8dab68"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#5B5B3F"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ABCE83"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#A4C67D"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#9BBF72"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#EBF4A4"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"color":"#87ae79"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#7f2200"},{"visibility":"off"}]},{"featureType":"administrative","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"},{"visibility":"on"},{"weight":4.1}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#495421"}]},{"featureType":"administrative.neighborhood","elementType":"labels","stylers":[{"visibility":"off"}]}],
         // Disable scrolling
         scrollwheel: false,
         navigationControl: false,
@@ -166,6 +75,158 @@ styles:
         title: ''
     });
 }
+
+/***********************************************
+* LightGallery - Lightbox with slideshow, captions and comments
+***********************************************/
+$(document).ready(function() {
+    // $("#lightgallery").lightGallery();
+
+    // Interior features
+    $('#dynamic').on('click', function() {
+
+    $(this).lightGallery({
+        dynamic: true,
+        dynamicEl: [{
+            "src": 'img/wild/int1.png',
+            'thumb': 'img/wild/int1.png',
+            'subHtml': '<h2>Specifications</h2>'
+        }, {
+            'src': 'img/wild/int2.jpg',
+            'thumb': 'img/wild/int2.jpg',
+            // 'subHtml': "<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>"
+        }, {
+            'src': 'img/wild/int3.jpg',
+            'thumb': 'img/wild/int3.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }, {
+            'src': 'img/wild/int4.jpg',
+            'thumb': 'img/wild/int4.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }, {
+            'src': 'img/wild/int5.jpg',
+            'thumb': 'img/wild/int5.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }]
+    })
+    //
+    });
+
+    // Exterior features
+    $('#dynamic2').on('click', function() {
+
+    $(this).lightGallery({
+        dynamic: true,
+        dynamicEl: [{
+            "src": 'img/wild/ext1.png',
+            'thumb': 'img/wild/ext1.png',
+            'subHtml': '<h2>Specifications</h2>'
+        }, {
+            'src': 'img/wild/ext2.jpg',
+            'thumb': 'img/wild/ext2.jpg',
+            // 'subHtml': "<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>"
+        }, {
+            'src': 'img/wild/ext3.jpg',
+            'thumb': 'img/wild/ext3.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }, {
+            'src': 'img/wild/ext4.jpg',
+            'thumb': 'img/wild/ext4.jpg',
+        }, {
+            'src': 'img/wild/ext5.jpg',
+            'thumb': 'img/wild/ext5.jpg',
+        }, {
+            'src': 'img/wild/ext6.jpg',
+            'thumb': 'img/wild/ext6.jpg',
+        }, {
+            'src': 'img/wild/ext7.jpg',
+            'thumb': 'img/wild/ext7.jpg',
+        }]
+    })
+    //
+    });
+
+    // Kitchen features
+    $('#dynamic3').on('click', function() {
+
+    $(this).lightGallery({
+        dynamic: true,
+        dynamicEl: [{
+            "src": 'img/wild/kit1.png',
+            'thumb': 'img/wild/kit1.png',
+            'subHtml': '<h2>Specifications</h2>'
+        }, {
+            'src': 'img/wild/kit2.jpg',
+            'thumb': 'img/wild/kit2.jpg',
+            // 'subHtml': "<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>"
+        }, {
+            'src': 'img/wild/kit3.jpg',
+            'thumb': 'img/wild/kit3.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }, {
+            'src': 'img/wild/kit4.jpg',
+            'thumb': 'img/wild/kit4.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }, {
+            'src': 'img/wild/kit5.jpg',
+            'thumb': 'img/wild/kit5.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }, {
+            'src': 'img/wild/kit6.jpg',
+            'thumb': 'img/wild/kit6.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }]
+    })
+    //
+    });
+
+    // Build features
+    $('#dynamic4').on('click', function() {
+
+    $(this).lightGallery({
+        dynamic: true,
+        dynamicEl: [{
+            "src": 'img/wild/build1.png',
+            'thumb': 'img/wild/build1.png',
+            'subHtml': '<h2>Specifications</h2>'
+        }, {
+            'src': 'img/wild/build2.png',
+            'thumb': 'img/wild/build2.png',
+            // 'subHtml': "<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>"
+        }, {
+            'src': 'img/wild/build3.jpg',
+            'thumb': 'img/wild/build3.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }, {
+            'src': 'img/wild/build4.jpg',
+            'thumb': 'img/wild/build4.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }, {
+            'src': 'img/wild/build5.jpg',
+            'thumb': 'img/wild/build5.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }, {
+            'src': 'img/wild/build6.jpg',
+            'thumb': 'img/wild/build6.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }, {
+            'src': 'img/wild/build7.jpg',
+            'thumb': 'img/wild/build7.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }, {
+            'src': 'img/wild/build8.jpg',
+            'thumb': 'img/wild/build8.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }, {
+            'src': 'img/wild/build9.jpg',
+            'thumb': 'img/wild/build9.jpg',
+            // 'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+        }]
+    })
+    //
+    });
+
+});
 
 /***********************************************
 * Intense - Image zooming
